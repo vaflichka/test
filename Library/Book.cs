9dbbd1bdc74f6f66a8e4423f898a53fe84eb7318
vaflichka <99564566+vaflichka@users.noreply.github.com>
2023-03-15 10:07:39 +0200

    Add files via upload

---
+++ Library/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        private string id;
+        private Book next;
+        public Book(string bookId)
+
+        {
+
+            this.id = bookId;
+            this.next = null;
+
+        }
+        public string BookId
+
+        {
+
+            get { return this.id; }
+            set { this.id = value; }
+
+        }
+
+
+
+        public Book Next
+
+        {
+
+            get { return this.Next; }
+            set { this.Next = value; }
+
+        }
+
+
+
+        public override string ToString()
+
+        {
+
+            return $"Book {this.id}";
+
+        }
+
+    }
+}
---
+++ Library/Shelf.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Shelf
+    {
+        private Book head;
+        private Book tail;
+        private int count;
+        public Shelf()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+            private set
+            {
+                this.count = value;
+            }
+        }
+        public void AddBook(string bookId)
+        {
+            if (this.head == null)
+            {
+                var next = new Book(bookId);
+                this.head = next;
+                this.tail = next;
+            }
+            else
+            {
+                var next = new Book(bookId);
+                tail.Next = next;
+                tail = next;
+            }
+
+            this.Count++;
+        }
+        public void AddSpecialBook(string bookId)
+        {
+            if (this.head == null)
+            {
+                var next = new Book(bookId);
+                this.head = next;
+                this.tail = next;
+            }
+            else
+            {
+
+                var next = new Book(bookId);
+                next.Next = head;
+                this.head = next;
+
+
+            }
+            this.Count++;
+        }
+
+        public Book CheckBookIsPresent(string bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReleaseBook(string bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReleaseBook(int index)
+        {
+
+
+        }
+
+        public StringBuilder ShelfInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (count > 0)
+            {
+                Book g = head;
+                for (int i = 0; i < count; i++)
+                {
+                    sb.AppendLine(g.ToString());
+                    g = g.Next;
+                }
+                return sb;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
